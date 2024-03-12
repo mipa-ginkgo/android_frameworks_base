999e9e30407e1d122251f53c2c60abdddfadc021
Hirokixd <86230878+Wrdn28@users.noreply.github.com>
2024-03-13 02:17:21 +0700

    make play integrity pass

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -406,14 +406,14 @@ public class PixelPropsUtils {
 
     private static void spoofBuildGms() {
         // Alter build parameters to avoid hardware attestation enforcement
-        setPropValue("BRAND", "motorola");
-        setPropValue("MANUFACTURER", "motorola");
-        setPropValue("DEVICE", "griffin");
-        setPropValue("ID", "MCC24.246-37");
-        setPropValue("FINGERPRINT", "motorola/griffin_retcn/griffin:6.0.1/MCC24.246-37/42:user/release-keys");
-        setPropValue("MODEL", "XT1650-05");
-        setPropValue("PRODUCT", "griffin_retcn");
-        setVersionFieldString("SECURITY_PATCH", "2016-07-01");
+        setBuildField("BRAND", "Hisense");
+                    setBuildField("MANUFACTURER", "Hisense");
+                    setBuildField("DEVICE", "HS6735MT");
+                    setBuildField("ID", "MRA58K");
+                    setBuildField("FINGERPRINT", "Hisense/F30/HS6735MT:6.0/MRA58K/L1228.6.01.01:user/release-keys");
+                    setBuildField("MODEL", "Hisense F30");
+                    setBuildField("PRODUCT", "F30");
+                    setVersionFieldString("SECURITY_PATCH", "2016-02-01");
     }
 
     private static boolean isCallerSafetyNet() {
