23111410e89debb44b4711c6d7f87080213dcf5f
YARA <122050843+Yara0202@users.noreply.github.com>
2024-03-13 07:21:57 +0000

    Revert "fixup! Introduce PixelPropsUtils for safety net spoof [SQUASHED]"
    This reverts commit 25ebbcb3572fa797c280c999fe54a91b0e4fa3ae.

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.custom;
+package com.android.internal.util.arrow;
 
 import android.app.Application;
 import android.os.Build;
