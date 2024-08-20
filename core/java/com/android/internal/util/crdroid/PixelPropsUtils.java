ee1aa3233aff74c1f4e0fa7b0b6a3fb1baffd4c0
YARA <122050843+Yara0202@users.noreply.github.com>
2024-08-20 18:17:41 +0700

    PixelPropsUtils: Update Fingerprint to akita

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -412,12 +412,12 @@ public class PixelPropsUtils {
         // Alter build parameters to avoid hardware attestation enforcement
         setPropValue("BRAND", "google");
         setPropValue("MANUFACTURER", "Google");
-        setPropValue("DEVICE", "sailfish");
-        setPropValue("ID", "OPM1.171019.011");
-        setPropValue("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-        setPropValue("MODEL", "Pixel");
-        setPropValue("PRODUCT", "sailfish");
-        setVersionFieldString("SECURITY_PATCH", "2017-12-05");
+        setPropValue("DEVICE", "akita");
+        setPropValue("ID", "AP31.240617.015");
+        setPropValue("FINGERPRINT", "google/akita_beta/akita:15/AP31.240617.015/12207491:user/release-keys");
+        setPropValue("MODEL", "Pixel 8a");
+        setPropValue("PRODUCT", "akita_beta");
+        setVersionFieldString("SECURITY_PATCH", "2024-08-05");
     }
 
     private static boolean isCallerSafetyNet() {
